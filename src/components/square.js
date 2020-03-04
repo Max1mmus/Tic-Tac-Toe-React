6a2413a564ae5935f4d6d372a5054b69a94350f5
Max1mmus <56004853+Max1mmus@users.noreply.github.com>
2020-03-04 22:45:12 +0100

    indentation fixed

--- src/components/square.js
+++ src/components/square.js
@@ -1,18 +1,17 @@
 import React from 'react';
 
 export function Square (props) {
-  const styles = {};
-  if (props.value !== null) {
-    styles.background = "yellow";
-    } 
-    return(
-      <button
-        className="square"
-        onClick={props.onClick}
-        style={styles}
-      >
-        {props.value}
-      </button>
-      );
+    const styles = {};
+    if (props.value !== null) {
+        styles.background = "yellow";
+    }
+    return (
+        <button
+            className="square"
+            onClick={props.onClick}
+            style={styles}
+        >
+            {props.value}
+        </button>
+    );
 }
-
