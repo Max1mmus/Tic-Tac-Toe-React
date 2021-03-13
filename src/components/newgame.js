19f0fd7956bf048d5dfe85cb61623f65fbace3c5
Max1mmus <56004853+Max1mmus@users.noreply.github.com>
2021-03-13 21:32:05 +0100

    stylistic issue fixed

--- src/components/newgame.js
+++ src/components/newgame.js
@@ -3,10 +3,8 @@ import React from 'react';
 export function NewGame (props) {
     return (
         <div className="buttonContainer">
-            <button className="newGame"
-                    onClick={props.onClick}
-            >
-            New Game
+            <button className="newGame" onClick={props.onClick}>
+                New Game
             </button>
         </div>
     );
