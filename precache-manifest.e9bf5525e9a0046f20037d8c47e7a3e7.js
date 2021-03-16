f41ca4404338fc2d349100db5eca125edfc5e85f
Max1mmus <56004853+Max1mmus@users.noreply.github.com>
2021-03-16 01:07:29 +0100

    Updates

---
+++ precache-manifest.e9bf5525e9a0046f20037d8c47e7a3e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beffbc6050a38ca33cafbe25eb00ae01",
+    "url": "/Tic-Tac-Toe-React/index.html"
+  },
+  {
+    "revision": "375e6faccaad908915fa",
+    "url": "/Tic-Tac-Toe-React/static/css/main.a080bbdd.chunk.css"
+  },
+  {
+    "revision": "c8945183b39d650a99a9",
+    "url": "/Tic-Tac-Toe-React/static/js/2.ea8f0682.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Tic-Tac-Toe-React/static/js/2.ea8f0682.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "375e6faccaad908915fa",
+    "url": "/Tic-Tac-Toe-React/static/js/main.1e22d32f.chunk.js"
+  },
+  {
+    "revision": "4fff186fc79d0dbf0de8",
+    "url": "/Tic-Tac-Toe-React/static/js/runtime-main.791ceffe.js"
+  }
+]);
